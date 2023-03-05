c8c43f1104b8c6a6f2b0a701e29581b6aa76fefd
Rameezkhan47 <rameezmuhammadkhan47@gmail.com>
2023-03-06 04:46:35 +0500

    added modal bottom sheet so that new transaction input field render only when a user press add icon

--- transactions_app/lib/main.dart
+++ transactions_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import './user_transactions.dart';
+
+import './new_transaction.dart';
+import './transactions_list.dart';
+import './model/transaction.dart';
 
 void main() => runApp(const MyApp());
 
@@ -23,9 +26,37 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+   final List<Transaction> _userTransaction = [
+    Transaction(id: 'T1', title: 'Shirt', amount: 3500, date: DateTime.now()),
+    Transaction(id: 'T2', title: 'Jeans', amount: 5000, date: DateTime.now()),
+    Transaction(id: 'T3', title: 'Shoes', amount: 18000, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+        title: title,
+        amount: amount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext context){ ///takes two arguments context and builder (context of which we want it to render when button is pressed)
+    showModalBottomSheet(context: context, builder: (_){ //builder is a function that returns the widget we need to be inside modal bottom sheet
+      return NewTransaction(_addNewTransaction);
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
             return GestureDetector(
@@ -35,11 +66,14 @@ class MyHomePage extends StatelessWidget {
     child: Scaffold(
       appBar: AppBar(
         title: const Text('Transactions App'),
+        actions:<Widget>[
+          IconButton(icon: const Icon(Icons.add), onPressed: () {_startAddNewTransaction(context);})
+        ],
       ),
       body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const <Widget>[
+          children:  <Widget>[
             // ignore: avoid_unnecessary_containers
             // Container(
             //   margin: const EdgeInsets.only(bottom: 5),
@@ -61,9 +95,10 @@ class MyHomePage extends StatelessWidget {
             //     ),
             //   ),
             // ),
-            Expanded(child: UserTransactions())
+             Expanded(child: TransactionList(_userTransaction))
           ],
         ),
+        floatingActionButton: FloatingActionButton(child: Icon(Icons.add),onPressed: (){_startAddNewTransaction(context);},),
       ),
     );
   }
