78b9514bc584571ace2bcf85b5852640798d76c4
Rameezkhan47 <rameezmuhammadkhan47@gmail.com>
2023-03-06 05:06:58 +0500

    converted new_transaction widget into a stateful widget in order to persist input data between flutter reevaluation

--- transactions_app/lib/new_transaction.dart
+++ transactions_app/lib/new_transaction.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
+class NewTransaction extends StatefulWidget { //converted to stateful widget to remove a bug where as soon as the user 
+//types into input field and unfocus, the data gets lost 
 
-class NewTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
   final Function addTx;
-  NewTransaction(this.addTx,{super.key});
+  const NewTransaction(this.addTx, {super.key});
 
-    // Hide the keyboard
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
 
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0){
+      return;
+    }
+    
+    widget.addTx(enteredTitle, enteredAmount); //enables us to access the property which is in different class NewTransaction
+    Navigator.of(context).pop(); //closes modal upon submission
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     return Card(
         elevation: 5,
         margin: const EdgeInsets.only(bottom: 20),
@@ -24,17 +39,19 @@ class NewTransaction extends StatelessWidget {
               TextField(
                 decoration: const InputDecoration(labelText: 'Title'),
                 controller: titleController,
+                onSubmitted: (_) => submitData(),
               ),
               TextField(
                 keyboardType: TextInputType.number,
                 decoration: const InputDecoration(labelText: 'Amount'),
                 controller: amountController,
+                onSubmitted: (_) => submitData(),
               ),
               OutlinedButton(
                 style: TextButton.styleFrom(foregroundColor: Colors.purple),
                 onPressed: () {
-                  addTx(titleController.text,
-                  double.parse(amountController.text));
+                  submitData();
+                  
                 },
                 child: const Text('Add Transaction'),
               ),
