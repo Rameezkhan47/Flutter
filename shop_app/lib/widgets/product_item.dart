ed349771ac57472c6fb9c2448e5966d87abc6128
Rameezkhan47 <rameezmuhammadkhan47@gmail.com>
2023-03-17 12:36:40 +0500

    snackbar that will let you know if item is successfully added to cart

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -6,45 +6,60 @@ import '../providers/cart.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  // final String id;
-  // final String title;
-  // final String imageUrl;
-
-  // ProductItem(this.id, this.title, this.imageUrl);
-  // not using instance variables as we are using provider approach
-
-  const ProductItem({super.key});
+  ProductItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context,
-        listen: false); //wont listen to notifications
-            final cart = Provider.of<Cart>(context, listen: false);
-
+    final product =
+        Provider.of<Product>(context, listen: false); //won't listen to notifications
+    final cart = Provider.of<Cart>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: Consumer<Product>(
-              builder: (context, value, child) => IconButton(
-                  onPressed: () => product.toggleFavoriteStatus(),
-                  color: Theme.of(context).colorScheme.error,
-                  icon: Icon(product.isFavorite
-                      ? Icons.favorite_rounded
-                      : Icons.favorite_outline_rounded)
-                      ),
-
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            builder: (context, value, child) => IconButton(
+              onPressed: () => product.toggleFavoriteStatus(product.id as String),
+              color: Theme.of(context).colorScheme.error,
+              icon: Icon(product.isFavorite
+                  ? Icons.favorite_rounded
+                  : Icons.favorite_outline_rounded),
             ),
-            title: Text(
-              product.title as String, style: Theme.of(context).textTheme.titleSmall, 
-              textAlign: TextAlign.center,
+          ),
+          title: Text(
+            product.title as String,
+            style: Theme.of(context).textTheme.titleSmall,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {
+              cart.addItem(
+                product.id as String,
+                product.price as double,
+                product.title as String,
+              );
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Added item to cart!'),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeItem(product.id as String);
+                    },
+                  ),
+                ),
+              );
+            },
+            icon: Icon(
+              Icons.add_shopping_cart_rounded,
+              color: Theme.of(context).colorScheme.primary,
             ),
-            trailing: IconButton(
-                onPressed: () {cart.addItem(product.id as String, product.price as double, product.title as String);},
-                icon: Icon(Icons.add_shopping_cart_rounded,
-                    color: Theme.of(context).colorScheme.primary))),
+          ),
+        ),
         child: GestureDetector(
           onTap: () => Navigator.of(context)
               .pushNamed(ProductDetailScreen.routeName, arguments: product.id),
