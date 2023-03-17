84bfd00eaca2a1fd4223095c68e4ce3ba44f660a
Rameezkhan47 <rameezmuhammadkhan47@gmail.com>
2023-03-17 12:35:11 +0500

    user products will be fetched from backend with CRUD functionality

--- shop_app/lib/screens/user_products_screen.dart
+++ shop_app/lib/screens/user_products_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
 
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
@@ -9,36 +12,63 @@ import './edit_product_screen.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final productsData = Provider.of<Products>(context);
+    final productsData = Provider.of<Products>(context, listen: false);
+
+    final PreferredSizeWidget appBar = AppBar(
+      title: const Text('Your Products'),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: () {
+            Navigator.of(context).pushNamed(EditProductScreen.routeName);
+          },
+        ),
+      ],
+    );
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Products'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+      appBar: appBar,
+      drawer: AppDrawer(),
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: FutureBuilder(
+            future: _refreshProducts(context),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text('An error occurred!'),
+                );
+              }
+
+              return Consumer<Products>(
+                builder: (context, value, child) {
+                  return ListView.builder(
+                    itemCount: productsData.items.length,
+                    itemBuilder: (_, i) => Column(
+                      children: [
+                        UserProductItem(
+                          productsData.items[i].id,
+                          productsData.items[i].title,
+                          productsData.items[i].imageUrl,
+                        ),
+                        Divider(),
+                      ],
+                    ),
+                  );
+                },
+              );
             },
           ),
-        ],
-      ),
-      drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: productsData.items.length,
-          itemBuilder: (_, i) => Column(
-                children: [
-                  UserProductItem(
-                    productsData.items[i].id as String,
-                    productsData.items[i].title as String,
-                    productsData.items[i].imageUrl as String,
-                  ),
-                  Divider(),
-                ],
-              ),
         ),
       ),
     );
