d3e17661c3837f2d3c8b5e6de4d0740668b742e2
Rameezkhan47 <rameezmuhammadkhan47@gmail.com>
2023-03-17 12:30:48 +0500

    Future logic for deleting products from firebase

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -121,11 +121,21 @@ class Products with ChangeNotifier { //mixin with ChangeNotifier that gives us a
     }
   }
 
-    void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+  Future<void> deleteProduct(String id) async {
+    final url =
+        'https://flutter-41360-default-rtdb.firebaseio.com/products/$id.json';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    Product? existingProduct = _items[existingProductIndex];
+
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+    final response = await http.delete(Uri.parse(url));
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Could not delete product');
+    }
+    existingProduct = null; //clearing from memory
   }
-
-
 }
 
