411205f567c2cdf0cbb7571254f4831325cc8178
Rameezkhan47 <rameezmuhammadkhan47@gmail.com>
2023-03-17 12:27:11 +0500

    added Futures for fetching and deleting Orders from firebase

--- shop_app/lib/providers/orders.dart
+++ shop_app/lib/providers/orders.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 import './cart.dart';
+import 'package:shop_app/models/http_exception.dart';
 
 class OrderItem {
   final String id;
@@ -23,16 +26,90 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List cartProducts, double total) {
+  Future<void> fetchAndSetOrders() async {
+    const url = 'https://flutter-41360-default-rtdb.firebaseio.com/orders.json';
+    final response = await http.get(Uri.parse(url));
+
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+
+    if (extractedData == null) {
+      
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          dateTime: DateTime.parse(orderData['dateTime']),
+          products: (orderData['products'] as List<dynamic>)
+              .map(
+                (item) => CartItem(
+                  id: item['id'],
+                  price: item['price'],
+                  quantity: item['quantity'],
+                  title: item['title'],
+                ),
+              )
+              .toList(),
+        ),
+      );
+
+
+    }
+    
+    );
+    
+
+    _orders = loadedOrders.reversed.toList();
+
+    notifyListeners();
+  }
+
+  Future<void> addOrder(List cartProducts, double total) async {
+    const url = 'https://flutter-41360-default-rtdb.firebaseio.com/orders.json';
+    final timeStamp = DateTime.now();
+    final response = await http.post(Uri.parse(url),
+        body: json.encode({
+          'amount': total,
+          'dateTime': timeStamp.toIso8601String(),
+          'products': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price
+                  })
+              .toList()
+        }));
+
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: json.decode(response.body)['name'],
         amount: total,
-        dateTime: DateTime.now(),
+        dateTime: timeStamp,
         products: cartProducts,
       ),
     );
     notifyListeners();
+  }
+
+  Future<void> deleteOrder(String id) async {
+    final url =
+        'https://flutter-41360-default-rtdb.firebaseio.com/orders/$id.json';
+    final existingOrderIndex = _orders.indexWhere((order) => order.id == id);
+    OrderItem? existingOrder = _orders[existingOrderIndex];
+
+    _orders.removeAt(existingOrderIndex);
+    notifyListeners();
+    final response = await http.delete(Uri.parse(url));
+    if (response.statusCode >= 400) {
+      _orders.insert(existingOrderIndex, existingOrder);
+      notifyListeners();
+      throw HttpException('Could not delete Order');
+    }
+    existingOrder = null; //clearing from memory
   }
 }
