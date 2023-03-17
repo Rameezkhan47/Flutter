ac85e73bd8894543f6813b83a990d7de99998332
Rameezkhan47 <rameezmuhammadkhan47@gmail.com>
2023-03-17 12:27:35 +0500

    favorite status of products will now be fetched from firebase

--- shop_app/lib/providers/product.dart
+++ shop_app/lib/providers/product.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
 
-class Product with ChangeNotifier { ///mixin with ChangeNotifier that gives us access to nethod notifyListeners()
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class Product with ChangeNotifier {
+  ///mixin with ChangeNotifier that gives us access to nethod notifyListeners()
   final String? id;
   final String title;
   final String description;
@@ -17,8 +21,30 @@ class Product with ChangeNotifier { ///mixin with ChangeNotifier that gives us a
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus(String id) async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url =
+        'https://flutter-41360-default-rtdb.firebaseio.com/products/$id.json';
+    try {
+      final response = await http.patch(
+        Uri.parse(url),
+        body: json.encode({'isFavorite': isFavorite}),
+      );
+      if (response.statusCode >= 400) {
+        print('old status');
+        _setFavValue(oldStatus);
+      }
+      print('all ok');
+    } catch (e) {
+      print('error');
+      _setFavValue(oldStatus);
+    }
   }
 }
