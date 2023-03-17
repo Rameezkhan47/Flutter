430439e7db9ea631f78907a52be0ba791248cacb
Rameezkhan47 <rameezmuhammadkhan47@gmail.com>
2023-03-17 12:30:20 +0500

    Future logic for adding products to firebase

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -103,12 +103,22 @@ class Products with ChangeNotifier { //mixin with ChangeNotifier that gives us a
     notifyListeners();
   }
 
-    void updateProduct(String? id, Product newProduct) {
+  Future<void> updateProduct(String? id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url =
+          'https://flutter-41360-default-rtdb.firebaseio.com/products/$id.json';
+      await http.patch(Uri.parse(url),
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price
+          }));
+
       _items[prodIndex] = newProduct;
       notifyListeners();
-    } 
+    }
   }
 
     void deleteProduct(String id) {
